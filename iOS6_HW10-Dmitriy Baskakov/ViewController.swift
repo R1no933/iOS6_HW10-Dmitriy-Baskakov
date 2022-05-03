fc513a6cbd68ca9a8f03844d63810eaa64fc0a9f
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-05-03 09:01:31 +0300

    Initial Commit

---
+++ iOS6_HW10-Dmitriy Baskakov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6_HW10-Dmitriy Baskakov
+//
+//  Created by Dmitriy Baskakov on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
