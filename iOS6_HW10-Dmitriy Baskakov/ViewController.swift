40736687569b818c352e9ff8baba99669f0918be
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-05-03 09:26:40 +0300

    clear comment
    Deleted all comments in project

--- iOS6_HW10-Dmitriy Baskakov/ViewController.swift
+++ iOS6_HW10-Dmitriy Baskakov/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  iOS6_HW10-Dmitriy Baskakov
-//
-//  Created by Dmitriy Baskakov on 03.05.2022.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     func chengeViewColor() {
